e1eb5f3d764935d55c0ec00eafcff7638fb67a65
umerindented <umerfiaz251@gmail.com>
2023-01-19 20:55:56 -0700

    changes

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ except ImportError:
     print("No module named 'google' found")
 
 # to search
-query = "Huawei contact us"
+company_name = input('Enter company name: ')
+query = company_name + ' contact us'
 goodsites = []
-for j in search(query, num=10, stop=10, pause=2):
+for j in search(query, num=8, stop=8, pause=2):
     if '/ca/' and 'contact' in j:
         goodsites.append(j)
 
 for u in range(0, len(goodsites)):
     session = HTMLSession()
-    r = session.get(goodsites[u])
-    r.html.render(timeout=20)
+    getsites = session.get(goodsites[u])
+    getsites.html.render(timeout=20)
 
     emails = re.findall(
         "([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", r.html.raw_html.decode())
